22380d6691c908f0880952c16d1b4d03a1eba8ba
selenerdogannn <selennerdogann252@gmail.com>
2025-10-06 22:08:46 +0300

    adding skill page

---
+++ portfolioProject/Controllers/SkillController.cs
@@ -0,0 +1,49 @@
+﻿using portfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace portfolioProject.Controllers
+{
+    public class SkillController : Controller
+    {
+        blogdbEntities context = new blogdbEntities();
+
+        // GET: 
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            return View(value);
+        }
+    }
+}
