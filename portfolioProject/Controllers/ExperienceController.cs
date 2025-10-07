b4096f71c9a8e9eaa34cb3a3761c49095016bef9
selenerdogannn <selennerdogann252@gmail.com>
2025-10-07 19:59:40 +0300

    adding experience page

---
+++ portfolioProject/Controllers/ExperienceController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Ajax.Utilities;
+using portfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace portfolioProject.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        blogdbEntities context = new blogdbEntities();
+        public ActionResult ExperienceList()
+        {
+            var values = context.Experience.ToList();
+            return View(values);
+            
+        }
+        [HttpGet]
+
+        public ActionResult CreateExperience()
+        {
+           
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateExperience(Experience experience)
+        {
+            context.Experience.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        
+        public ActionResult DeleteExperience(int id)
+        {
+            var value = context.Experience.Find(id);
+            context.Experience.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpPost]
+
+        public ActionResult UpdateExperience(int id) { 
+            var value=context.Experience.Find(id);
+            return View(value);
+
+        }
+
+        [HttpPost]
+
+        public ActionResult UpdateExperience(Experience experience)
+        {
+            var value = context.Experience.Find(experience);
+            value.Title = experience.Title;
+            value.Subtitle = experience.Subtitle;
+            value.Description = experience.Description;
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+
+        }
+    }
+}
